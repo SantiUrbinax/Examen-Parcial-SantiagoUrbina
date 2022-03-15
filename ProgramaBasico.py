41a08a424c3cc17891566b0bd3521d710588fc64
Santi Urbina <urbinasanti1@gmail.com>
2022-03-15 09:44:22 -0600

    ProgramaBasico 0.02
    Programa basico de pyhton actualizado con la info del inge Miguel, junto con mi nombre y mi carnet.

--- ProgramaBasico.py
+++ ProgramaBasico.py
@@ -4,3 +4,6 @@ nombre = input("Ingrese su nombre: ")
 print(f"Hola {nombre}, tenga buen dia :D")
 
 print("\nFelicidades, va muy bien con su evaluación parcial. Att. Ing. Miguel")
+
+print("Angel Santiago Urbina Lopez")
+print("Carnet: 1546122")
